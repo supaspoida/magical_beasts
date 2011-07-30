28a1b81d3b78cc73e81dc98111a42d464d30fd2a
Lar Van Der Jagt <dev+supaspoida@hashrocket.com>
2011-07-29 20:11:00 -0400

    Generate rails app
    - rails new presents -O -T

--- Gemfile
+++ Gemfile
@@ -1,4 +1,15 @@
 source 'http://rubygems.org'
 
 gem 'draper'
+gem 'jquery-rails'
 gem 'rails', '3.1.0.rc5'
+
+group :assets do
+  gem 'sass-rails', "~> 3.1.0.rc"
+  gem 'coffee-rails', "~> 3.1.0.rc"
+  gem 'uglifier'
+end
+
+group :development, :test do
+  gem 'ruby-debug19', require: 'ruby-debug', platform: 'ruby_19'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Presents::Application.config.secret_token = '724db175523ee2f5b190b9c9f438d23c89da679afbe133420e73e5ffa3342c98a5161ea41c23cf05534f60446bc06d25a3cb172bbcd00ce060a808df9dd1b6d4'
